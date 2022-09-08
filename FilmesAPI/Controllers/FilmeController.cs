261cd616157e416ed192c7a2c19dea122c4f3fd0
Pedro Schiavom <pedro.schiavon@mcs-it.com.br>
2022-09-07 22:37:40 -0300

    creating project

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,25 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace FilmesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
